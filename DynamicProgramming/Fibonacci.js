e812141cd7572880a522bf3cab688b3caed96f8b
Levi Ermonaites <leviermonaitesdefreitas@gmail.com>
2021-01-27 12:31:46 -0300

    How many shaked hands challenge

--- DynamicProgramming/Fibonacci.js
+++ DynamicProgramming/Fibonacci.js
@@ -8,4 +8,4 @@ const fib = (n) => {
     return result;
 }
 
-console.log(fib(6));
+console.log(fib(10000));
---
+++ Recursion/ShakedHands.js
@@ -0,0 +1,12 @@
+/*
+In a party of N people, each person will shake her/his hand with each other person only once. On total how many hand-shakes would happen?
+*/
+
+const howManyShakedHands = (n) => {
+    let shakedHands;
+    if(n <= 1) return shakedHands = 0;
+    else { shakedHands = n - 1};
+    return shakedHands + howManyShakedHands(shakedHands);
+}
+
+console.log(howManyShakedHands(4));
