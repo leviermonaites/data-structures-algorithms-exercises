52ada024a49d81df899577efb6c51f366d22dd9e
Levi Ermonaites <leviermonaitesdefreitas@gmail.com>
2021-02-11 11:41:25 -0300

    Changing some little things in the uniValSubTrees challenge

--- Recursion/IsTreeUniVal.js
+++ Recursion/IsTreeUniVal.js
@@ -2,22 +2,22 @@ const Node = require('../NodeClass');
 
 const node1 = new Node(1, null);
 
-const node2 = new Node(0, node1);
-const node3 = new Node(1, node1);
+const node2 = new Node(2, node1);
+const node3 = new Node(3, node1);
 
-const node4 = new Node(1, node2);
-const node5 = new Node(0, node2);
+const node4 = new Node(4, node2);
+const node5 = new Node(5, node2);
 
-const node6 = new Node(0, node3);
-const node7 = new Node(1, node3);
+const node6 = new Node(6, node3);
+const node7 = new Node(7, node3);
 
-const howManyUniValTrees = (root, univaltrees = 0) => {
-    if(root === null) return univaltrees += 1;
-    if(!root.left && !root.right) return univaltrees += 1;
-    if(root.left && root.right && root.data === root.left.data && root.data === root.right.data) univaltrees += 1;
-    univaltrees = howManyUniValTrees(root.left, univaltrees);
-    univaltrees = howManyUniValTrees(root.right, univaltrees);
-    return univaltrees;
-}
+const howManyUniValSubTrees = (root, univalsubtrees = 0) => {
+  if (root === null) return (univalsubtrees += 1);
+  if (!root.left && !root.right) return (univalsubtrees += 1);
+  if (root.left.data === root.data && root.right.data === root.data) univalsubtrees += 1;
+  univalsubtrees = howManyUniValSubTrees(root.left, univalsubtrees);
+  univalsubtrees = howManyUniValSubTrees(root.right, univalsubtrees);
+  return univalsubtrees;
+};
 
-console.log(howManyUniValTrees(node1));
+console.log(howManyUniValSubTrees(node1));
