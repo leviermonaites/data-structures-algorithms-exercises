b24bd918a9286670125bc32c8431bcbcc4a8d55f
Levi Ermonaites <leviermonaitesdefreitas@gmail.com>
2021-02-20 07:27:35 -0300

    Providing README files to the scripts

---
+++ Recursion/Fibonacci/README.md
@@ -0,0 +1,4 @@
+#   HELPER
+
+Hello there!
+I just created that file, to make you aware that I'll provide an explanation of it
---
+++ Recursion/LinkedLists/RegularLinkedList/index.js
@@ -0,0 +1,34 @@
+class Node {
+    static lastCreated;
+    constructor(data) {
+      this.data = data;
+      this.next = void{};
+      if(Node.lastCreated) {
+          Node.lastCreated.next = this;
+      }
+      Node.lastCreated = this;
+    }
+  
+    static countNodes(head) {
+      let count = head.data ? 1 : 0;
+      if (head.next) {
+        let currentNode = head;
+        do {
+          console.log('The current Node data is: ', currentNode.data);
+          console.log('The next Node data is: ', currentNode.next.data);
+          currentNode = currentNode.next;
+          count += 1;
+        } while (currentNode.next);
+      }
+      console.log('The total of nodes is: ', count);
+      return count;
+    }
+  }
+  
+  const head = new Node(4);
+  const nodeB = new Node(8);
+  const nodeC = new Node(11);
+  const nodeD = new Node(17);
+  
+  Node.countNodes(head);
+  
