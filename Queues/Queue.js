7faf44488d53065af66b3d3d88f376088ae861b2
Levi Ermonaites <leviermonaitesdefreitas@gmail.com>
2021-03-08 15:12:42 -0300

    Separating class code and testing code

---
+++ Queues/Queue.js
@@ -0,0 +1,68 @@
+import QueueInterface from "./QueueInterface.js";
+
+class Queue extends QueueInterface {
+  constructor(length) {
+    super(length);
+  }
+
+  enqueue(element) {
+    if (this.isFull) return "Queue already full!";
+    this.elements[this.rear] = element;
+    this.updateValues("enq");
+    return this.elements;
+  }
+
+  dequeue() {
+    this.elements[this.front] = null;
+    this.updateValues("dq");
+    return this.elements;
+  }
+
+  moveFront(to) {
+    switch (to) {
+      case this.totalLength:
+        return 0;
+      case -1:
+        return this.totalLength - 1;
+      default:
+        return to;
+    }
+  }
+
+  moveRear(to) {
+    switch (to) {
+      case this.totalLength:
+        if (!this.elements[0]) return 0;
+      case -1:
+        return this.totalLength - 1;
+      default:
+        return to;
+    }
+  }
+
+  updateValues(method) {
+    switch (method) {
+      case "enq": {
+        this.rear = this.moveRear(this.rear + 1);
+        break;
+      }
+
+      case "dq": {
+        this.front = this.moveFront(this.front + 1);
+        break;
+      }
+    }
+    this.checkIfQueueIsFull();
+    return;
+  }
+
+  checkIfQueueIsFull() {
+    if (this.rear >= this.totalLength || this.elements[this.rear + 1]) {
+      this.isFull = true;
+    } else this.isFull = false;
+  }
+}
+
+export const createNewQueue = (length) => new Queue(length);
+
+export default Queue;
