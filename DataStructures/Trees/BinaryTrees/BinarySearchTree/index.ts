57cd432674b87dba2d767593d4f38098a8f68cc8
Levi Ermonaites De Freitas <68472819+leviermonaites@users.noreply.github.com>
2021-07-14 08:47:05 -0300

    Implementing BFS as a BST method

--- DataStructures/Trees/BinaryTrees/BinarySearchTree/index.ts
+++ DataStructures/Trees/BinaryTrees/BinarySearchTree/index.ts
@@ -1,9 +1,46 @@
 import BinaryTree from "../interface/BinaryTree.ts";
 import BinaryNode from "../BinaryNode.ts";
+import Queue from '../../../Queues/SinglyLinkedListBasedQueue.ts';
+
 
 class BST implements BinaryTree {
   root: BinaryNode | null = null;
   private size = 0;
+  
+  get length() {
+    return this.size;
+  }
+
+  bfs() {
+    const nodes = new Array(this.length);
+    
+    const queue = new Queue();
+    queue.enqueue(this.root);
+    
+    for(let i = 0; i < nodes.length; i++) {
+      const node = queue.dequeue() as BinaryNode;
+      if(node.left) queue.enqueue(node.left);
+      if(node.right) queue.enqueue(node.right);
+      nodes[i] = node.value;
+    }
+  
+    return nodes;
+  }
+
+  find(value: number) {
+    if(this.root && this.root.value === value) return this.root;
+    else {
+      let trav = this.root;
+
+      while(trav) {
+        if(value > trav.value) trav = trav.right;
+        else if(value < trav.value) trav = trav.left;
+        else break;
+      }
+      
+      return trav;
+    }
+  }
 
   insert(value: number) {
     const node = new BinaryNode(value);
@@ -32,24 +69,8 @@ class BST implements BinaryTree {
     return this.root;
   }
 
-  find(value: number) {
-    if(this.root && this.root.value === value) return this.root;
-    else {
-      let trav = this.root;
 
-      while(trav) {
-        if(value > trav.value) trav = trav.right;
-        else if(value < trav.value) trav = trav.left;
-        else break;
-      }
-      
-      return trav;
-    }
-  }
 
-  get length() {
-    return this.size;
-  }
 }
 
 export default BST;
